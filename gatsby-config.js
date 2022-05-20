92eaed9c139d1133beb25295ea4f5a8598f4314f
James Reagan <james@jpreagan.com>
2022-05-19 18:35:14 -1000

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  siteMetadata: {
+    title: `James Reagan`,
+    description: `My personal website.`,
+    author: `James Reagan`,
+    siteUrl: `https://jpreagan.com/`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-image`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `James Reagan`,
+        short_name: `James Reagan`,
+        start_url: `/`,
+        background_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`,
+      },
+    },
+  ],
+}
