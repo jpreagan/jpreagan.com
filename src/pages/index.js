0c8a8abf02aba33394a5b627266fc5a100eda06d
James Reagan <james@jpreagan.com>
2022-05-20 17:48:28 -1000

    Remove placeholder text

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,16 +7,6 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <Heading
-    as="h1"
-    sx={{
-      fontSize: 6,
-    }}
-
-    >
-      Heading 1
-      </Heading>
-    <p>Hello, World!</p>
   </Layout>
 )
 
