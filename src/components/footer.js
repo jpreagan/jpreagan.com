a2a92069e3c600a7a02008d6d91b48a5f96d88c8
James Reagan <james@jpreagan.com>
2022-05-29 18:17:23 -1000

    Add spacing to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,10 @@
-import * as React from "react"
+/** @jsx jsx */
+import { jsx } from "theme-ui"
 import PropTypes from "prop-types"
 
 const Footer = ({ author }) => {
   return (
-    <footer>
+    <footer sx={{ mb: 4 }}>
       {author} © {new Date().getFullYear()}
     </footer>
   )
