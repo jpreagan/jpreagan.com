01b278432a46dbcf1c8a32d68ec3a50e4de5d71d
James Reagan <james@jpreagan.com>
2022-07-12 18:23:50 -1000

    Add Cypress

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,17 @@ module.exports = {
   globals: {
     __PATH_PREFIX__: true,
   },
-  extends: ["airbnb", "airbnb-typescript", "prettier"],
+  extends: [
+    "airbnb",
+    "airbnb-typescript",
+    "prettier",
+    "plugin:cypress/recommended",
+  ],
   parserOptions: {
     project: "./tsconfig.eslint.json",
+  },
+  rules: {
+    "react/jsx-props-no-spreading": 0,
+    "react/forbid-prop-types": 0,
   },
 }
