e9a6321db5104cc8125cb4028dcfb48d0d761f04
James Reagan <james@jpreagan.com>
2022-08-09 17:16:31 -1000

    Add fonts

---
+++ gatsby-browser.ts
@@ -0,0 +1,6 @@
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/800.css";
+import "@fontsource/source-sans-pro/400.css";
+import "@fontsource/source-sans-pro/700.css";
+import "@fontsource/source-sans-pro/400-italic.css";
+import "@fontsource/source-sans-pro/700-italic.css";
