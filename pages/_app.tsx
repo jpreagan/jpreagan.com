171ab3009e3dd97475cf4894e3f41459868eb9a2
James Reagan <james@jpreagan.com>
2022-09-13 11:26:58 -1000

    Add display and body fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,11 @@
 import React from "react";
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/800.css";
+import "@fontsource/ibm-plex-sans/400.css";
+import "@fontsource/ibm-plex-sans/700.css";
+import "@fontsource/ibm-plex-sans/400-italic.css";
+import "@fontsource/ibm-plex-sans/700-italic.css";
+import "@fontsource/ibm-plex-mono/";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
