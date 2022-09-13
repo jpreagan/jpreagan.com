3c88e147f5bb00df24be153451c55dd54b8f00dd
James Reagan <james@jpreagan.com>
2022-09-13 12:33:14 -1000

    Update footer component

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Link from "next/link";
 
 export default function Footer() {
   const year = new Date().getFullYear();
 
-  return <footer>James Reagan © {year}</footer>;
+  return (
+    <footer className="mx-auto my-8 max-w-5xl px-4 text-base md:text-lg lg:text-xl">
+      <Link href="/">
+        <a className="hover:underline">James Reagan</a>
+      </Link>{" "}
+      © {year}
+    </footer>
+  );
 }
