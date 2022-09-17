37620757fcc933f07e39fd107b53a5131ddbf942
James Reagan <james@jpreagan.com>
2022-09-17 12:54:19 -1000

    Set language

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="en">
       <Head />
       <body className="bg-white font-body text-gray-700 dark:bg-gray-900 dark:text-gray-400">
         <Main />
