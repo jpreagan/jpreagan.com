8af9caabdc1a5ce1d45e8aea3b3902b33b618186
James Reagan <james@jpreagan.com>
2022-09-27 10:40:06 -1000

    Add unit testing with Jest

---
+++ __tests__/aloha.test.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Aloha from "../components/aloha";
+
+describe("Aloha component", () => {
+  it("renders a heading", () => {
+    render(<Aloha />);
+
+    const heading = screen.getByRole("heading", {
+      name: /aloha/i,
+    });
+
+    expect(heading).toBeInTheDocument();
+  });
+});
