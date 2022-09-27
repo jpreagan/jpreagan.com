e02aa40895c61a0c32a869f94a1d3fa5580f5bc6
James Reagan <james@jpreagan.com>
2022-09-27 10:10:19 -1000

    Fix mismatch with theme toggle

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,8 +7,8 @@ import "@fontsource/ibm-plex-sans/700.css";
 import "@fontsource/ibm-plex-sans/400-italic.css";
 import "@fontsource/ibm-plex-sans/700-italic.css";
 import "@fontsource/ibm-plex-mono/400.css";
-import "@theme-toggles/react/css/Expand.css";
 import "../styles/globals.css";
+import "../styles/expand.css";
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
