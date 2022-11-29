3487c7e93b34255d022c1e54e7c64916ded01966
James Reagan <james@jpreagan.com>
2022-11-28 17:20:25 -1000

    Add link to blog page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Head from "next/head";
+import Link from "next/link";
 import Layout from "../components/layout";
 import Aloha from "../components/aloha";
 import Posts from "../components/posts";
@@ -36,6 +37,16 @@ export default function IndexPage({ allPostsData }: Props) {
           Recent posts
         </h2>
         <Posts allPostsData={allPostsData} />
+        <p className="my-8 md:text-lg lg:text-xl">
+          ✨ See{" "}
+          <Link
+            className="dark:text-sky:300 text-sky-600 underline"
+            href="/blog"
+          >
+            all posts
+          </Link>
+          .
+        </p>
       </section>
       <section className="my-16 mx-auto max-w-5xl px-4 lg:my-32">
         <h2 className="mb-8 text-2xl font-bold text-gray-900 dark:text-gray-100 md:text-3xl lg:text-4xl">
