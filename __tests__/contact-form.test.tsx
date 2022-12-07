431dc01c7d48fbd8636db10c7f40841ba7a35db2
James Reagan <james@jpreagan.com>
2022-12-06 20:09:44 -1000

    Add test for contact form

---
+++ __tests__/contact-form.test.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import ContactForm from "../components/contact-form";
+
+jest.mock("@formspree/react", () => {
+  const originalModule = jest.requireActual("@formspree/react");
+  return {
+    ...originalModule,
+    useForm: jest.fn(() => [{}, jest.fn()]),
+  };
+});
+
+test("renders an input of type text", () => {
+  render(<ContactForm />);
+  const nameInput = screen.getByLabelText(/name/i);
+  expect(nameInput).toHaveAttribute("type", "text");
+});
+test("renders an input of type email", () => {
+  render(<ContactForm />);
+  const emailInput = screen.getByLabelText(/email/i);
+  expect(emailInput).toHaveAttribute("type", "email");
+});
+test("renders a textarea for the message", () => {
+  render(<ContactForm />);
+  const messageTextarea = screen.getByLabelText(/message/i);
+  expect(messageTextarea).toBeInstanceOf(HTMLTextAreaElement);
+});
