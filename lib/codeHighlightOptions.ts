2ec4a447bba68204036a6323c9b03e44ddeaaa81
James Reagan <james@jpreagan.com>
2023-03-16 19:25:08 -1000

    Use Tokyo Night theme

--- lib/codeHighlightOptions.ts
+++ lib/codeHighlightOptions.ts
@@ -1,6 +1,10 @@
+import fs from "fs";
 import { type Options } from "rehype-pretty-code";
 
 export const codeHighlightOptions: Partial<Options> = {
+  theme: JSON.parse(
+    fs.readFileSync("./lib/themes/tokyo-night-color-theme.json", "utf-8")
+  ),
   onVisitLine(node) {
     if (node.children.length === 0) {
       node.children = [{ type: "text", value: " " }];
