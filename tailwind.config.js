ae5c6a5e54b15bd2e8617c3f342d5891c9bba013
James Reagan <james@jpreagan.com>
2023-04-15 08:55:57 -1000

    Add keyframe animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,29 @@ module.exports = {
       colors: {
         gray: colors.zinc,
       },
+      keyframes: {
+        appear: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+        tada: {
+          "0%": { transform: "scale3d(1, 1, 1)" },
+          "10%, 20%": {
+            transform: "scale3d(0.9, 0.9, 0.9) rotate3d(0, 0, 1, -3deg)",
+          },
+          "30%, 50%, 70%, 90%": {
+            transform: "scale3d(1.1, 1.1, 1.1) rotate3d(0, 0, 1, 3deg)",
+          },
+          "40%, 60%, 80%": {
+            transform: "scale3d(1.1, 1.1, 1.1) rotate3d(0, 0, 1, -3deg)",
+          },
+          "100%": { transform: "scale3d(1, 1, 1)" },
+        },
+      },
+      animation: {
+        appear: "appear 1s ease-in-out",
+        tada: "tada 1s ease-in-out",
+      },
       letterSpacing: {
         tight: "-0.015em",
       },
