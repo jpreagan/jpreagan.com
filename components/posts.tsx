d846d67d95c215ec88f8ec717413efd44a49291b
James Reagan <james@jpreagan.com>
2023-04-14 22:46:11 -1000

    Make cards equal height for posts

--- components/posts.tsx
+++ components/posts.tsx
@@ -4,7 +4,10 @@ import Date from "~/components/date";
 
 export default function Posts({ posts }: { posts: Post[] }) {
   return (
-    <div className="grid grid-cols-1 gap-4 md:grid-cols-2" role="grid">
+    <div
+      className="grid auto-rows-fr grid-cols-1 gap-4 md:grid-cols-2"
+      role="grid"
+    >
       {posts.map((post, index: number) => {
         const even =
           "bg-gradient-to-r from-purple-900 to-pink-700 hover:from-purple-800 hover:to-pink-600";
@@ -17,7 +20,7 @@ export default function Posts({ posts }: { posts: Post[] }) {
             key={post.slug}
             href={`/blog/${post.slug}`}
             itemProp="url"
-            className={`rounded-md px-4 py-16 shadow-sm transition-transform duration-300 ease-in-out hover:-translate-y-1.5 md:px-10 ${linearGradient}`}
+            className={`flex items-center justify-center rounded-md px-4 py-16 shadow-sm transition-transform duration-300 ease-in-out hover:-translate-y-1.5 md:px-10 ${linearGradient}`}
           >
             <article itemScope itemType="http://schema.org/Article">
               <header>
