89c298b108b927e73fe3700a34db9ec7f4b6ef19
James Reagan <james@jpreagan.com>
2023-04-21 16:08:40 -1000

    Add newsletter signup to blog posts

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -3,6 +3,7 @@ import { notFound } from "next/navigation";
 import { allPosts } from "contentlayer/generated";
 import Date from "~/components/date";
 import Mdx from "~/components/mdx";
+import NewsletterSignup from "~/components/newsletter-signup";
 
 export async function generateStaticParams() {
   return allPosts.map((post) => ({
@@ -52,28 +53,28 @@ export default async function BlogPost({
   }
 
   return (
-    <>
-      <article itemScope itemType="http://schema.org/Article">
-        <header className="mx-auto mb-8 max-w-5xl sm:px-4">
-          <div className="bg-gradient-to-r from-purple-900 via-pink-700 to-orange-500 px-4 py-16 sm:rounded-md md:px-8 md:py-24 lg:px-16 lg:py-32">
-            <h1
-              itemProp="headline"
-              className="mb-4 text-2xl font-bold leading-tight text-gray-200 sm:text-3xl sm:leading-tight md:text-5xl md:leading-tight lg:text-6xl lg:leading-tighter"
-            >
-              {post.title}
-            </h1>
-            <p className="text-gray-300 md:text-lg lg:text-xl">
-              <Date dateString={post.date} />
-            </p>
-          </div>
-        </header>
-        <section
-          itemProp="articleBody"
-          className="prose mx-auto my-4 px-4 dark:prose-invert md:prose-lg lg:prose-xl prose-a:text-sky-300 lg:my-16"
-        >
-          <Mdx code={post.body.code} />
-        </section>
-      </article>
-    </>
+    <article itemScope itemType="http://schema.org/Article">
+      <header className="mx-auto max-w-5xl sm:px-4">
+        <div className="bg-gradient-to-r from-purple-900 via-pink-700 to-orange-500 px-4 py-16 sm:rounded-md md:px-8 md:py-24 lg:px-16 lg:py-32">
+          <h1
+            itemProp="headline"
+            className="mb-4 text-2xl font-bold leading-tight text-gray-200 sm:text-3xl sm:leading-tight md:text-5xl md:leading-tight lg:text-6xl lg:leading-tighter"
+          >
+            {post.title}
+          </h1>
+          <p className="text-gray-300 md:text-lg lg:text-xl">
+            <Date dateString={post.date} />
+          </p>
+        </div>
+      </header>
+      <section
+        itemProp="articleBody"
+        className="prose mx-auto my-16 px-4 dark:prose-invert md:prose-lg lg:prose-xl prose-a:text-sky-300 lg:my-32"
+      >
+        {/* className="mx-auto my-16 max-w-5xl px-4 lg:my-32" */}
+        <Mdx code={post.body.code} />
+      </section>
+      <NewsletterSignup />
+    </article>
   );
 }
