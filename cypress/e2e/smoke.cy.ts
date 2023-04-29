a07123d88f318dbae86427448798c273f20079fa
James Reagan <james@jpreagan.com>
2023-04-29 11:23:59 -1000

    Update smoke test

--- cypress/e2e/smoke.cy.ts
+++ cypress/e2e/smoke.cy.ts
@@ -1,14 +1,12 @@
 describe("Smoke test", () => {
-  it("should visit the index page and the blog page", () => {
+  it("should visit the index page and the about page", () => {
     cy.visit("/")
       .get("main")
       .should("exist")
-      .contains("✨ See all posts.")
-      .find("a")
+      .get('a[href="/about"]')
+      .contains(/about me/i)
       .click()
       .get("main")
       .should("exist");
   });
 });
-
-export {};
