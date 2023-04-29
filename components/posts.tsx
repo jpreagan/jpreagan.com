cf1d88bf76df11acf606730753cd54664d6cea49
James Reagan <james@jpreagan.com>
2023-04-29 11:01:03 -1000

    Refactor posts layout

--- components/posts.tsx
+++ components/posts.tsx
@@ -1,43 +1,20 @@
-import Link from "next/link";
-import { Post } from "contentlayer/generated";
+import { Post } from "~/.contentlayer/generated";
 import Date from "~/components/date";
 
 export default function Posts({ posts }: { posts: Post[] }) {
   return (
-    <div
-      className="grid auto-rows-fr grid-cols-1 gap-4 md:grid-cols-2"
-      role="grid"
-    >
-      {posts.map((post, index: number) => {
-        const even =
-          "bg-gradient-to-r from-purple-900 to-pink-700 hover:from-purple-800 hover:to-pink-600";
-        const odd =
-          "bg-gradient-to-r from-pink-700 to-orange-500 hover:from-pink-600 hover:to-orange-400";
-        const linearGradient = index % 2 === 0 ? even : odd;
-
-        return (
-          <Link
-            key={post.slug}
-            href={`/blog/${post.slug}`}
-            itemProp="url"
-            className={`flex items-center justify-center rounded-md px-4 py-16 shadow-sm transition-transform duration-300 ease-in-out hover:-translate-y-1.5 md:px-10 ${linearGradient}`}
-          >
-            <article itemScope itemType="http://schema.org/Article">
-              <header>
-                <h2
-                  itemProp="headline"
-                  className="mb-4 text-xl font-bold tracking-tight text-gray-200 md:text-2xl"
-                >
-                  {post.title}
-                </h2>
-                <p className="text-gray-300">
-                  <Date dateString={post.date} />
-                </p>
-              </header>
-            </article>
-          </Link>
-        );
-      })}
+    <div className="mx-auto grid grid-cols-1 gap-x-8 gap-y-16 lg:grid-cols-2">
+      {posts.map((post) => (
+        <article key={post.slug}>
+          <div className="text-sm text-gray-500">
+            <Date dateString={post.date} />
+          </div>
+          <h3 className="mt-3 text-xl font-bold leading-6 text-gray-100">
+            <a href={`/blog/${post.slug}`}>{post.title}</a>
+          </h3>
+          <p className="mt-3">{post.description}</p>
+        </article>
+      ))}
     </div>
   );
 }
