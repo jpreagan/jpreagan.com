6ddccafe3f599e0e50e47d57d072f2af2a587469
James Reagan <james@jpreagan.com>
2023-04-29 10:59:36 -1000

    Use Tailwind CSS forms plugin for contact form

--- components/contact-form.tsx
+++ components/contact-form.tsx
@@ -25,7 +25,7 @@ export default function ContactForm() {
         name="name"
         autoComplete="name"
         aria-label="Name"
-        className="mb-4 block w-full rounded-md border-none bg-gray-800 p-2 text-gray-100 shadow-sm"
+        className="mb-4 w-full min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-pink-300 sm:text-sm sm:leading-6"
       />
       <ValidationError prefix="Name" field="name" errors={state.errors} />
       <label htmlFor="email" className="mb-1 block">
@@ -37,7 +37,7 @@ export default function ContactForm() {
         name="email"
         autoComplete="email"
         aria-label="Email Address"
-        className="mb-4 block w-full rounded-md border-none bg-gray-800 p-2 text-gray-100 shadow-sm"
+        className="mb-4 w-full min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-pink-300 sm:text-sm sm:leading-6"
       />
       <ValidationError prefix="Email" field="email" errors={state.errors} />
       <label htmlFor="message" className="mb-1 block">
@@ -48,13 +48,13 @@ export default function ContactForm() {
         name="message"
         rows={10}
         aria-label="Message"
-        className="mb-4 block w-full rounded-md border-none border-gray-500 bg-gray-800 p-2 text-gray-100 shadow-sm"
+        className="mb-4 w-full min-w-0 flex-auto rounded-md border-0 bg-white/5 px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-pink-300 sm:text-sm sm:leading-6"
       />
       <ValidationError prefix="Message" field="message" errors={state.errors} />
       <button
         type="submit"
         disabled={state.submitting}
-        className="rounded-md bg-pink-600 bg-gradient-to-r from-purple-800 to-pink-600 px-5 py-4 font-semibold text-gray-100 shadow-sm hover:bg-pink-500 hover:from-purple-700 hover:to-pink-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-600"
+        className="rounded-md bg-pink-600 bg-gradient-to-r from-purple-800 to-pink-600 px-3.5 py-2.5 font-semibold text-white shadow-sm hover:bg-pink-500 hover:from-purple-700 hover:to-pink-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-pink-300"
       >
         Submit
       </button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -90,5 +90,5 @@ module.exports = {
       }),
     },
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
 };
