45761a8661c24a2969c96daf84c5fd15bc37587b
James Reagan <james@jpreagan.com>
2023-10-25 17:09:26 -1000

    Move date inside component

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,6 +1,6 @@
-const today = new Date();
-
 export default function Footer() {
+  const today = new Date();
+
   return (
     <footer className="site-footer">
       <a href="/">James Reagan</a> © {today.getFullYear()}
