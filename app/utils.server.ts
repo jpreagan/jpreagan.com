75208a21875f9983c302ec15ca8ba91122234e14
James Reagan <james@jpreagan.com>
2023-11-01 18:00:53 -1000

    Add utilities

--- app/utils.server.ts
+++ app/utils.server.ts
@@ -1,5 +1,27 @@
 import { buildImageUrl } from "cloudinary-build-url";
 import { bundleMDX } from "mdx-bundler";
+import validator from "validator";
+
+const DEFAULT_REDIRECT = "/";
+
+export function safeRedirect(
+  to: FormDataEntryValue | string | null | undefined,
+  defaultRedirect: string = DEFAULT_REDIRECT,
+) {
+  if (!to || typeof to !== "string") {
+    return defaultRedirect;
+  }
+
+  if (!to.startsWith("/") || to.startsWith("//")) {
+    return defaultRedirect;
+  }
+
+  return to;
+}
+
+export function validateEmail(email: unknown): email is string {
+  return validator.isEmail(email as string);
+}
 
 export const getImageUrls = (imageUrl: string, widths: number[]): string => {
   return widths
