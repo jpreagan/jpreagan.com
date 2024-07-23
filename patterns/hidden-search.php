d302cf4cdd1932a4f8c4d81185f3da2753bd18b1
James Reagan <james@jpreagan.com>
2024-07-22 17:01:56 -1000

    Move to child theme

--- patterns/hidden-search.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Title: Search
- * Slug: jpreagan/hidden-search
- * Inserter: no
- */
-?>
-
-<!-- wp:search {"label":"<?php echo esc_attr_x( 'Search', 'search form label', 'jpreagan' ); ?>","showLabel":false,"buttonText":"<?php echo esc_attr_x( 'Search', 'search button text', 'jpreagan' ); ?>","fontSize":"medium"} /-->
