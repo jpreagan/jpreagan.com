67cab30100daea76e1abc763d27a56b522bbfd3e
James Reagan <james@jpreagan.com>
2024-07-23 14:08:53 -1000

    Update styles

--- functions.php
+++ functions.php
@@ -15,7 +15,7 @@ function jpreagan_styles() {
 
 	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css', array(), $parent_version );
 
-	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'parent-style' ), '1.0.0' );
+	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'parent-style' ), '1.0.1' );
 }
 
 add_action( 'wp_enqueue_scripts', 'jpreagan_styles' );
