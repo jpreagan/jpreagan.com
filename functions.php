3116e8accae2accb20aacb310d819bc9561d86e2
James Reagan <james@jpreagan.com>
2024-07-25 10:37:07 -1000

    Use version from styles.css

--- functions.php
+++ functions.php
@@ -12,10 +12,12 @@
 function jpreagan_styles() {
 	$parent_theme   = wp_get_theme( get_template() );
 	$parent_version = $parent_theme->get( 'Version' );
+	$child_theme    = wp_get_theme();
+	$child_version  = $child_theme->get( 'Version' );
 
 	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css', array(), $parent_version );
 
-	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'parent-style' ), '1.0.1' );
+	wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'parent-style' ), $child_version );
 }
 
 add_action( 'wp_enqueue_scripts', 'jpreagan_styles' );
